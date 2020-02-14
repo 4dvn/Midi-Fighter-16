cd65b270e1d8b2cf7d13f91e659eba47f121e59e
4dvn <vuha.hau@hyper.vn>
2020-02-14 23:15:36 +0700

    Update Midi Fighter 16.ino

--- Firmware/Midi Fighter 16.ino
+++ Firmware/Midi Fighter 16.ino
@@ -18,10 +18,10 @@ CCPotentiometer potentiometer4(A3, {MIDI_CC::Channel_Volume, CHANNEL_5});
 
 // The note numbers corresponding to the buttons in the matrix
 const AddressMatrix<4, 4> addresses = {{
-  {48, 49, 50, 51},// Ableton drumrack map
-  {44, 45, 46, 47},
-  {40, 41, 42, 43},
-  {36, 37, 38, 39},
+  {51, 50, 49, 48},// Ableton drumrack map
+  {47, 46, 45, 44},
+  {43, 42, 41, 40},
+  {39, 38, 37, 36},
 }};
 
 NoteButtonMatrix<4, 4> buttonmatrix = {
