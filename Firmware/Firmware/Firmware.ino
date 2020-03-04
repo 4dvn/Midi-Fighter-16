e3d5108bd10c2e3b59a8768d6b8444db9da25065
4dvn <vuha.hau@hyper.vn>
2020-03-04 20:09:12 +0700

    Update

---
+++ Firmware/Firmware/Firmware.ino
@@ -0,0 +1,41 @@
+/*
+ Name:		Firmware.ino
+ Created:	3/4/2020 8:00:17 PM
+ Author:	Staal3D & 4D
+*/
+
+#include <Control_Surface.h>
+
+USBMIDI_Interface midi;
+// Fours potentiometer to analog pinout
+
+CCPotentiometer potentiometer1(A0, { MIDI_CC::Channel_Volume, CHANNEL_2 });
+
+CCPotentiometer potentiometer2(A1, { MIDI_CC::Channel_Volume, CHANNEL_3 });
+
+CCPotentiometer potentiometer3(A2, { MIDI_CC::Channel_Volume, CHANNEL_4 });
+
+CCPotentiometer potentiometer4(A3, { MIDI_CC::Channel_Volume, CHANNEL_5 });
+
+// The note numbers corresponding to the buttons in the matrix
+const AddressMatrix<4, 4> addresses = { {
+  {48, 49, 50, 51},// Ableton drumrack map
+  {44, 45, 46, 47},
+  {40, 41, 42, 43},
+  {36, 37, 38, 39},
+} };
+
+NoteButtonMatrix<4, 4> buttonmatrix = {
+  {2, 3, 4, 5}, // row pins
+  {6, 7, 8, 9},    // column pins
+  addresses,    // address matrix
+  CHANNEL_1,    // channel and cable number
+};
+
+void setup() {
+    Control_Surface.begin();
+}
+
+void loop() {
+    Control_Surface.loop();
+}
